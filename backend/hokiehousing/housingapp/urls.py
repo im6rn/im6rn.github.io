bcae0d50b5b61d9ade92c31d1851d67dcebea676
Victor Londono <vlondon2@gmu.edu>
2024-09-15 01:36:30 -0400

    WHY DO I KEEP FORGTTTIN TO BRANCH`

--- backend/hokiehousing/housingapp/urls.py
+++ backend/hokiehousing/housingapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('quiz-submit/', views.submit_quiz, name='submit_quiz'),
+    path('get-listings/', views.get_apartment_listings, name='get_apartment_listings')
     
 ]
