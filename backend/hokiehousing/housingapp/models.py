579c8cc46f49ba3ac2971afbc8318e333b0132cc
Victor Londono <vlondon2@gmu.edu>
2024-09-15 01:01:32 -0400

    FULLY LOCKING IN

--- backend/hokiehousing/housingapp/models.py
+++ backend/hokiehousing/housingapp/models.py
@@ -3,25 +3,11 @@ from django.db import models
 # Create your models here.
 
 class UserPreferences(models.Model):
-    on_campus = models.BooleanField(null=True, blank=True)
-    suite_style = models.BooleanField(default = False)
-    in_suite_bath = models.BooleanField(default=False)
-    desired_price = models.IntegerField(default = 0)
-    max_price = models.IntegerField(default = 2000)
-    utilities_included = models.BooleanField(default=False)
+    desired_price = models.IntegerField(default=0)
+    utilities = models.BooleanField(default=False)
+    bedrooms = models.IntegerField(default=1)
+    bathrooms = models.IntegerField(default=1)
+    amenities = models.JSONField(default=list)
+    public_transit = models.BooleanField(default=False)
+    
 
-    living_learning_community = models.JSONField(default=list)
-
-    ac = models.BooleanField(default=False)
-    public_transport = models.BooleanField(default=False)
-
-    desired_amenities = models.JSONField(default=list)
-
-    furnished = models.BooleanField(default=False)
-    desired_bedrooms = models.IntegerField()
-    desired_bathrooms = models.IntegerField()
-
-    desired_distance_from_campus = models.FloatField()
-    max_distance_from_campus = models.FloatField()
-
-    importance = models.JSONField()
--- backend/hokiehousing/housingapp/views.py
+++ backend/hokiehousing/housingapp/views.py
@@ -6,81 +6,20 @@ import json
 
 from .models import UserPreferences
 
-sample_apts = [
-    {
-        "complex": "Halstead Fair Oaks",
-        "address": "4576 Hunt Club Circle, Fairfax, VA 22033",
-        "on_campus": False,
-        "in_suite_bath": True,
-        "price": 2200,
-        "utilities_included": True,
-        "living_learning_community": [],
-        'ac': True,
-        "public_transport": True,
-        "amenities": [
-            "pool",
-            "washer/dryer",
-            "trash"
-        ],
-        "furnished": False,
-        "bathrooms": 2,
-        "bedrooms": 2,
-        "distance_from_campus": 5
-    },
-    {
-        "complex": "The Knoll at Fair Oaks",
-        "address": "5555 Pender Creek Circle, Fairfax, VA 22033",
-        "on_campus": False,
-        "in_suite_bath": True,
-        "price": 2900,
-        "utilities_included": True,
-        "living_learning_community": [],
-        'ac': True,
-        "public_transport": True,
-        "amenities": [
-            "pool",
-            "washer/dryer",
-            "trash",
-            "mewhenthe"
-        ],
-        "furnished": False,
-        "bathrooms": 2,
-        "bedrooms": 3,
-        "distance_from_campus": 7
-    }
-]
 
 @csrf_exempt
 def submit_quiz(request):
     try:
         data = json.loads(request.body)
         
-        # Check if the student is living on-campus or off-campus
-        on_campus = data.get('on_campus', False)
-
-        if on_campus:
-            # On-campus: Use only relevant fields
-            preferences = UserPreferences.objects.create(
-                on_campus=True,
-                in_suite_bath=data.get('in_suite_bath'),
-                ac=data.get('ac'),
-                living_learning_community=data.get('living_learning_community', []),
-                # Add more on-campus specific fields
-            )
-        else:
-            # Off-campus: Use the off-campus relevant fields
-            preferences = UserPreferences.objects.create(
-                on_campus=False,
-                desired_price=data.get('desired_price'),
-                utilities_included=data.get('utilities_included'),
-                public_transport=data.get('public_transport'),
-                desired_amenities=data.get('desired_amenities', []),
-                furnished=data.get('furnished'),
-                desired_bathrooms=data.get('desired_bathrooms'),
-                desired_bedrooms=data.get('desired_bedrooms'),
-                desired_distance_from_campus=data.get('desired_distance_from_campus'),
-                # Add more off-campus specific fields
-            )
+        preferences = UserPreferences.objects.create(
+            desired_price = data["desired_price"],
+            utilities = data["utilities"],
+            bedrooms = data['bedrooms'],
+            bathrooms = data["bathrooms"],
+            amenities = data['amenities'],
+            public_transit = data['public_transit']
+        )
 
         return JsonResponse({'status': 'success', 'user_token': preferences.id})
     except Exception as e:
