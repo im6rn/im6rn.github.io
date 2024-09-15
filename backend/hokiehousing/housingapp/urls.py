532b29c88875d53572f86991208e0252ee1175cf
Victor Londono <vlondon2@gmu.edu>
2024-09-15 01:05:53 -0400

    wtffffff

--- backend/hokiehousing/housingapp/urls.py
+++ backend/hokiehousing/housingapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('backend/hokiehousing/housingapp/quiz-submit/', views.submit_quiz, name='submit_quiz'),
+    path('housingapp/quiz-submit/', views.submit_quiz, name='submit_quiz'),
     
 ]
