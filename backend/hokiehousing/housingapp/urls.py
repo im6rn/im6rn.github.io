205ad57477f69615a65e0c441c5c3bf2b81ab21f
Victor Londono <vlondon2@gmu.edu>
2024-09-15 02:43:45 -0400

    bruh

--- backend/hokiehousing/housingapp/urls.py
+++ backend/hokiehousing/housingapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('quiz-submit/', views.submit_quiz, name='submit_quiz'),
-    path('get-listings/', views.get_apartment_listings, name='get_apartment_listings')
+    path('get-listings/', views.get_apartment_listings, name='get_apartment_listings'),
+    path('get-details/', views.get_apartment_details, name='get_apartment_details'),
     
 ]
